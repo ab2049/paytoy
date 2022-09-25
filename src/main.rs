3a6f645cbca01a9d08d83f7e5d150ed7d29bd4fc
A B <ab333971@gmail.com>
2022-09-25 12:35:36 +0100

    Add clap arg parsing
    Test plan:
    $ cargo run -- --help
        Finished dev [unoptimized + debuginfo] target(s) in 0.36s
         Running `target/debug/paytoy --help`
    paytoy
    Simple example payments engine
    
    USAGE:
        paytoy <INPUT>
    
    ARGS:
        <INPUT>    Input CSV file with header row: type, client, tx, amount
    
    OPTIONS:
        -h, --help    Print help information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(name = "paytoy", about = "Simple example payments engine")]
+struct Args {
+    /// Input CSV file with header row: type, client, tx, amount
+    #[clap(required = true)]
+    input: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
 }
